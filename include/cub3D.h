d2ace84139f724c30f90e5e741debaee8bdde424
Marc <Nope>
2023-06-23 20:14:14 +0200

    get texture filenames

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: mjourno <mjourno@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 11:10:11 by mjourno           #+#    #+#             */
-/*   Updated: 2023/06/23 15:47:17 by mjourno          ###   ########.fr       */
+/*   Updated: 2023/06/23 19:59:35 by mjourno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,19 @@
 # define CLOSE "Could'nt close file"
 # define EMPTY "Empty file"
 # define MALLOC "Malloc failed"
+# define DOUBLE_TXT "Same texture appears twice in map"
+# define MISS_TXT "Texture(s) missing"
 
 //Colors
 # define DEF		"\033[0;39m"
-# define GRAY		"\033[0;90m"
+//# define GRAY		"\033[0;90m"
 # define RED		"\033[0;91m"
-# define GREEN		"\033[0;92m"
+//# define GREEN		"\033[0;92m"
 # define YELLOW		"\033[0;93m"
-# define BLUE		"\033[0;94m"
-# define MAGENTA	"\033[0;95m"
-# define CYAN		"\033[0;96m"
-# define WHITE		"\033[0;97m"
+//# define BLUE		"\033[0;94m"
+//# define MAGENTA	"\033[0;95m"
+//# define CYAN		"\033[0;96m"
+//# define WHITE		"\033[0;97m"
 
 //include lib
 # include <stdio.h> //printf
--- src/main.c
+++ src/main.c
@@ -6,20 +6,23 @@
 /*   By: mjourno <mjourno@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 11:09:18 by mjourno           #+#    #+#             */
-/*   Updated: 2023/06/23 15:56:06 by mjourno          ###   ########.fr       */
+/*   Updated: 2023/06/23 20:13:36 by mjourno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
 
+//Prints debug info if DEBUG = 1
+//Then prints error
 int	print_err(char *fi, int li, const char *fu, char *str)
 {
 	if (DEBUG)
 		printf(YELLOW"File: %s | Line: %d | Function: %s\n"DEF, fi, li, fu);
-	printf(RED"Error\n %s\n"DEF, str);
+	printf(RED"Error\n%s\n"DEF, str);
 	return (1);
 }
 
+//Checks if filename ends in ".cub"
 int	wrong_filename(char *filename)
 {
 	int	i;
@@ -32,6 +35,7 @@ int	wrong_filename(char *filename)
 	return (0);
 }
 
+//Goes 1 by 1 char throug the file to get its length
 int	len_map(char *filename)
 {
 	int		i;
@@ -58,6 +62,7 @@ int	len_map(char *filename)
 	return (i - 1);
 }
 
+//Gets file content and put it in data->map
 int	read_file(char *filename, t_cub3D *data)
 {
 	int	len;
@@ -83,18 +88,51 @@ int	read_file(char *filename, t_cub3D *data)
 	return (0);
 }
 
-//int	get_textures(t_cub3D *data)
-//{
-//	int	i;
+//Puts filename in the right data->texture
+int	fname(t_cub3D *data, int i, int text)
+{
+	int	j;
 
-//	i = 0;
-//	while (data->map[i])
-//	{
-//		if ()
-//		i++;
-//	}
-//	return (0);
-//}
+	if (data->textures[text])
+		return (print_err(__FILE__, __LINE__, __func__, DOUBLE_TXT));
+	j = 0;
+	while (data->map[i + j] && data->map[i + j] != '\n')
+		j++;
+	data->textures[text] = ft_substr(data->map, i, j);
+	if (!data->textures[text])
+		return (print_err(__FILE__, __LINE__, __func__, MALLOC));
+	return (0);
+}
+
+//Get all texture paths
+int	get_texture_paths(t_cub3D *data)
+{
+	int	i;
+
+	i = 0;
+	while (data->map[i])
+	{
+		if (!ft_strncmp(data->map + i, "NO ", 3) && fname(data, i + 3, 0))
+				return (1);
+		else if (!ft_strncmp(data->map + i, "SO ", 3) && fname(data, i + 3, 1))
+			return (1);
+		else if (!ft_strncmp(data->map + i, "WE ", 3) && fname(data, i + 3, 2))
+			return (1);
+		else if (!ft_strncmp(data->map + i, "EA ", 3) && fname(data, i + 3, 3))
+			return (1);
+		else if (!ft_strncmp(data->map + i, "F ", 2) && fname(data, i + 2, 4))
+			return (1);
+		else if (!ft_strncmp(data->map + i, "C ", 2) && fname(data, i + 2, 5))
+			return (1);
+		else
+			i++;
+	}
+	i = -1;
+	while (++i < 6)
+		if (!data->textures[i])
+			return (print_err(__FILE__, __LINE__, __func__, MISS_TXT));
+	return (0);
+}
 
 int	parsing(t_cub3D *data, int argc, char **argv)
 {
@@ -104,8 +142,8 @@ int	parsing(t_cub3D *data, int argc, char **argv)
 		return (print_err(__FILE__, __LINE__, __func__, FNAME_FORMAT));
 	if (read_file(argv[1], data))
 		return (1);
-	//if (get_textures(data))
-	//	return (1);
+	if (get_texture_paths(data))
+		return (1);
 	return (0);
 }
 
@@ -119,14 +157,31 @@ void	init_data(t_cub3D *data)
 		data->textures[i] = NULL;
 }
 
+int	free_all(t_cub3D *data, int ret)
+{
+	int	i;
+
+	if (data->map)
+		free(data->map);
+	i = -1;
+	while (++i < 6)
+		if (data->textures[i])
+			free(data->textures[i]);
+	return (ret);
+}
+
 int	main(int argc, char **argv)
 {
 	t_cub3D	data;
 
 	init_data(&data);
 	if (parsing(&data, argc, argv))
-		return (1);
-	printf("%s\n",data.map);
-	free(data.map);
-	return (0);
+		return (free_all(&data, 1));
+
+	//debug texture name
+	int	j = -1;
+	while (++j < 6)
+		if (data.textures[j])
+			printf("%s\n", data.textures[j]);
+	free_all(&data, 0);
 }
