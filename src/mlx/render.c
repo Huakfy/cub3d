9e1230e5613d482b67363e576b6f76d7b883c6bb
Marc <Nope>
2023-06-30 16:44:06 +0200

    all size textures

--- src/mlx/render.c
+++ src/mlx/render.c
@@ -6,7 +6,7 @@
 /*   By: mjourno <mjourno@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 11:15:17 by mjourno           #+#    #+#             */
-/*   Updated: 2023/06/29 14:00:13 by mjourno          ###   ########.fr       */
+/*   Updated: 2023/06/30 16:43:30 by mjourno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,7 @@ static void	norm3(t_rayc *ray, t_map *data)
 	ray->lineHeight = (int)(HEIGHT / ray->perpWallDist);
 }
 
-static void	norm4(t_rayc *ray, t_mlx *mlx)
+static void	norm4(t_rayc *ray)
 {
 	ray->drawStart = -ray->lineHeight / 2 + HEIGHT / 2;
 	if(ray->drawStart < 0)
@@ -97,26 +97,43 @@ static void	norm4(t_rayc *ray, t_mlx *mlx)
 	else
 		ray->wallX = ray->posX + ray->perpWallDist * ray->rayDirX;
 	ray->wallX -= floor((ray->wallX));
-	ray->texX = (int)(ray->wallX * mlx->textures[0].width);
-	if(ray->side == 0 && ray->rayDirX > 0)
-		ray->texX = mlx->textures[0].width - ray->texX - 1;
-	if(ray->side == 1 && ray->rayDirY < 0)
-		ray->texX = mlx->textures[0].width - ray->texX - 1;
-	ray->step = 1.0 * mlx->textures[0].height / ray->lineHeight;
-	ray->texPos = (ray->drawStart - HEIGHT / 2 + ray->lineHeight / 2) * \
-	ray->step;
-	if (ray->side == 0 && ray->rayDirX > 0)
-		ray->side = 1;
-	else if (ray->side == 1 && ray->rayDirY > 0)
-		ray->side = 2;
-	else if (ray->side == 1 && ray->rayDirY < 0)
-		ray->side = 3;
 }
 
-static void	norm5(t_rayc *ray, int x, t_mlx *mlx)
+static void	norm5(t_rayc *ray, t_mlx *mlx)
+{
+	if (ray->side == 0 && ray->rayDirX < 0)
+	{
+		ray->texX = mlx->textures[0].width - ray->texX - 1;
+		ray->texX = (int)(ray->wallX * mlx->textures[0].width);
+		ray->side = 0;
+	}
+	else if (ray->side == 0 && ray->rayDirX > 0)
+	{
+		ray->texX = mlx->textures[1].width - ray->texX - 1;
+		ray->texX = (int)(ray->wallX * mlx->textures[1].width);
+		ray->side = 1;
+	}
+	else if (ray->side == 1 && ray->rayDirY > 0)
+	{
+		ray->texX = mlx->textures[2].width - ray->texX - 1;
+		ray->texX = (int)(ray->wallX * mlx->textures[2].width);
+		ray->side = 2;
+	}
+	else if (ray->side == 1 && ray->rayDirY < 0)
+	{
+		ray->texX = mlx->textures[3].width - ray->texX - 1;
+		ray->texX = (int)(ray->wallX * mlx->textures[3].width);
+		ray->side = 3;
+	}
+}
+
+static void	norm6(t_rayc *ray, int x, t_mlx *mlx)
 {
 	int	y;
 
+	ray->step = 1.0 * mlx->textures[ray->side].height / ray->lineHeight;
+	ray->texPos = (ray->drawStart - HEIGHT / 2 + ray->lineHeight / 2) * \
+	ray->step;
 	y = -1;
 	while (++y < HEIGHT)
 	{
@@ -126,7 +143,8 @@ static void	norm5(t_rayc *ray, int x, t_mlx *mlx)
 			img_pix_put(&mlx->screen, x, y, mlx->FC[0]);
 		else
 		{
-			ray->texY = (int)ray->texPos & (mlx->textures[0].height - 1);
+			ray->texY = (int)ray->texPos & (mlx->textures[ray->side].height - \
+			1);
 			ray->texPos += ray->step;
 			ray->color = *(unsigned int *)(mlx->textures[ray->side].addr + \
 			(mlx->textures[ray->side].line_len * ray->texY + ray->texX * \
@@ -150,8 +168,9 @@ int	render_screen(t_mlx *mlx)
 		norm1(ray, x);
 		norm2(ray);
 		norm3(ray, data);
-		norm4(ray, mlx);
-		norm5(ray, x, mlx);
+		norm4(ray);
+		norm5(ray, mlx);
+		norm6(ray, x, mlx);
 		x++;
 	}
 	mlx_put_image_to_window(mlx->ptr, mlx->win, mlx->screen.img, 0, 0);
